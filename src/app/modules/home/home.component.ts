9d98a46a82c23eafd27f89ad081bc9cb21003e63
Amanda Leandro Soares Do Carmo <amandaleandrosoares@gmail.com>
2024-07-06 13:12:35 -0300

    feat(features): Adicionado o formulario reativo

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  })
+
+  signupForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  })
+
+  constructor(private formBuilder:FormBuilder) {
+  }
+
+  onSubmitLoginForm():void {
+    console.log('DADOS DO FORMULARIO', this.loginForm.value)
+  }
+
+  onSubmitSignupForm():void {
+    console.log('DADOS DO FORMULARIO', this.signupForm.value)
+  }
 }
