ef399de7ef4f2be31f3c1fff481bc89a37a4feb5
Yzak Carmo <yzak.carmo@gmail.com>
2024-07-09 18:51:21 -0300

    feat(components): Implementing lazy loading

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { DashboardHomeComponent } from './page/dashboard-home/dashboard-home.component';
+import {RouterModule} from "@angular/router";
+import {DASHBOARD_ROUTES} from "./dashboard.routing";
+import {SidebarModule} from "primeng/sidebar";
 
 
 
@@ -12,7 +15,10 @@ import { DashboardHomeComponent } from './page/dashboard-home/dashboard-home.com
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forChild(DASHBOARD_ROUTES),
+    //PrimeNg
+    SidebarModule
   ]
 })
 export class DashboardModule { }
