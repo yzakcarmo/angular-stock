9285def7c7ad699c8f1185fa97bad4926be44cc5
Amanda Leandro Soares Do Carmo <amandaleandrosoares@gmail.com>
2024-07-20 19:19:13 -0300

    feat(service/products): Creating functions to search products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./modules/home/home.component";
-import {DashboardHomeComponent} from "./modules/dashboard/page/dashboard-home/dashboard-home.component";
 import {AuthGuard} from "./guards/auth-guard.service";
 
 const routes: Routes = [
@@ -18,7 +17,15 @@ const routes: Routes = [
 
   {
     path:'dashboard',
-    loadChildren: () => import('./modules/dashboard/dashboard.module').then((m) => m.DashboardModule),
+    loadChildren: () => import('./modules/dashboard/dashboard.module')
+      .then((m) => m.DashboardModule),
+    canActivate: [AuthGuard]
+  },
+
+  {
+    path:'products',
+    loadChildren: () => import('./modules/products/products.module')
+      .then((m) => m.ProductsModule),
     canActivate: [AuthGuard]
   }
 ];
--- src/app/modules/products/page/products-home/products-home.component.ts
+++ src/app/modules/products/page/products-home/products-home.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subject, takeUntil} from "rxjs";
+import {ProductsService} from "../../../../services/products/products.service";
+import {ProductsDataTransferService} from "../../../../shared/services/products/products-data-transfer.service";
+import {Router} from "@angular/router";
+import {GetAllProductsResponse} from "../../../../models/interfaces/products/response/GetAllProductsResponse";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-products-home',
   templateUrl: './products-home.component.html',
   styleUrls: []
 })
-export class ProductsHomeComponent {
+export class ProductsHomeComponent implements OnInit,OnDestroy{
+  private readonly destroy$ = new Subject<void>();
+  public productsData: Array<GetAllProductsResponse> = [];
 
+  constructor(
+    private productsService: ProductsService,
+    private productsDtService: ProductsDataTransferService,
+    private router: Router,
+    private messageService: MessageService
+
+  ) {}
+
+  ngOnInit() {
+    this.getServiceProductsData();
+  }
+
+  getServiceProductsData(){
+    const productsLoaded = this.productsDtService.getProductsDatas();
+
+    if(productsLoaded.length > 0) {
+      this.productsData = productsLoaded;
+    } else this.getAPIProductsData();
+
+    console.log(this.productsData)
+  }
+
+  getAPIProductsData() {
+    this.productsService
+      .getAllProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if(response.length > 0) {
+            this.productsData = response;
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao busccar produtos',
+            life: 2500
+          })
+          this.router.navigate(['/dashboard']);
+        }
+      })
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
